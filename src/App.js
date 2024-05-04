2facef86d66b2c74ccc9e8fcc2affb8824b3c5ca
Sajidsabila <129641654+Sajidsabila@users.noreply.github.com>
2024-05-04 13:01:20 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
           />
         </div>
       ) : (
-        <div className="content">
+        <div className="container-fluid">
           <Navbar />
           <Home />
           <AboutPage />
